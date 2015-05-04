93964d1f421348fa915c0b4f1716efae6f2a3333
Siwady <siwady0908@unitec.edu>
2015-05-04 10:42:48 -0600

    Draw Border

--- kernel.c
+++ kernel.c
@@ -1,42 +1,56 @@
+#define Base2 0xB000
 void main()
 {
-	int i,j;
+	
+	int i,j,w;
 	int PosY=0x0;
+	char* Word="Hello World";
+	int Size=11;
+	int Base=0x8000;
+
+	
 	//Clear Console
-	for(i=0;i<20;i++)
+	for(i=0;i<30;i++)
 	{
 		for(j=0;j<160;j++)
 		{
-			putInMemory(0xB000, 0x8000+(j+1)+(i*80*2), 0x0);
-			putInMemory(0xB000, 0x8001+(j+1)+(i*80*2), 0x1);
+			
+			putInMemory(Base2, Base+(j)+(i*80*2), 0x0);
+			putInMemory(Base2, Base+1+(j)+(i*80*2), 0x1);
+			
 		}
 	}
+
+	//border
+	for(i=0;i<30;i++)
+	{
+		for(j=0;j<160;j++)
+		{
+			if(!(j>3 && j<156 && i>0 &&i<24))
+			{
+				putInMemory(Base2, Base+(j)+(i*80*2), '*');
+				putInMemory(Base2, Base+1+(j)+(i*80*2), 0x5);
+			}
+			
+		}
+	}	
 	
-	//print HelloWord 
+	//print Hello World 
+	
 	for(i=0;i<16;i++)
 	{
-		PosY=((i*80)*2);
-		putInMemory(0xB000, 0x8000+PosY, 'H');
-		putInMemory(0xB000, 0x8001+PosY, i);	
-		putInMemory(0xB000, 0x8002+PosY, 'e');
-		putInMemory(0xB000, 0x8003+PosY, i);	
-		putInMemory(0xB000, 0x8004+PosY, 'l');
-		putInMemory(0xB000, 0x8005+PosY, i);
-		putInMemory(0xB000, 0x8006+PosY, 'l');
-		putInMemory(0xB000, 0x8007+PosY, i);	
-		putInMemory(0xB000, 0x8008+PosY, 'o');
-		putInMemory(0xB000, 0x8009+PosY, i);	
-		putInMemory(0xB000, 0x800A+PosY, 'W');
-		putInMemory(0xB000, 0x800B+PosY, i);	
-		putInMemory(0xB000, 0x800C+PosY, 'o');
-		putInMemory(0xB000, 0x800D+PosY, i);	
-		putInMemory(0xB000, 0x800E, 'r');
-		putInMemory(0xB000, 0x800F+PosY, i);		
-		putInMemory(0xB000, 0x8010+PosY, 'd');
-		putInMemory(0xB000, 0x8011+PosY, i);		
-				
+		PosY=(((i+2)*80)*2);
+		for(w=0;w<Size;w++)
+		{
+			putInMemory(Base2, Base+PosY+10, Word[w]);
+			putInMemory(Base2, Base+1+PosY+10, i);
+			
+			Base+=2;
+		}
+		Base-=Size*2;
 	}
 		
 }
+
 
 
