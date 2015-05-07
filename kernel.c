e5e41d509c32de0a3805e109bfe396fb9d42cfb9
Siwady <siwady0908@unitec.edu>
2015-05-07 12:46:01 -0600

    Put Code in procedures

--- kernel.c
+++ kernel.c
@@ -1,56 +1,75 @@
 #define Base2 0xB000
+#define Base 0x8000
+
+void PrintBorder();
+void Clr();
+void PrintMessage(char* Word,int Size);
+
 void main()
 {
-	
-	int i,j,w;
-	int PosY=0x0;
 	char* Word="Hello World";
 	int Size=11;
-	int Base=0x8000;
-
 	
 	//Clear Console
-	for(i=0;i<30;i++)
-	{
-		for(j=0;j<160;j++)
-		{
-			
-			putInMemory(Base2, Base+(j)+(i*80*2), 0x0);
-			putInMemory(Base2, Base+1+(j)+(i*80*2), 0x1);
-			
-		}
-	}
-
+	Clr();
+	
 	//border
-	for(i=0;i<30;i++)
-	{
-		for(j=0;j<160;j++)
-		{
-			if(!(j>3 && j<156 && i>0 &&i<24))
-			{
-				putInMemory(Base2, Base+(j)+(i*80*2), '*');
-				putInMemory(Base2, Base+1+(j)+(i*80*2), 0x2);
-			}
-			
-		}
-	}	
-	
+	PrintBorder();
+
 	//print Hello World 
+	PrintMessage(Word,Size);
 	
-	for(i=0;i<16;i++)
-	{
-		PosY=(((i+2)*80)*2);
-		for(w=0;w<Size;w++)
-		{
-			putInMemory(Base2, Base+PosY+10, Word[w]);
-			putInMemory(Base2, Base+1+PosY+10, i);
-			
-			Base+=2;
-		}
-		Base-=Size*2;
-	}
-		
 }
 
+void PrintBorder()
+{
+	int i,j;
+	
+	for(i=0;i<30;i++)
+	{
+		for(j=0;j<160;j++)
+		{
+			if(!(j>3 && j<156 && i>0 && i<24))
+			{
+				putInMemory(Base2, Base+(j)+(i*80*2), '*');
+				putInMemory(Base2, Base+1+(j)+(i*80*2), 0x42);
+			}
+		}
+	}	
+}
+
+void Clr()
+{
+	int i,j;
+
+	for(i=0;i<30;i++)
+	{
+		for(j=0;j<160;j++)
+		{
+			putInMemory(Base2, Base+(j)+(i*80*2), 0x0);
+			putInMemory(Base2, Base+1+(j)+(i*80*2), 0x1);
+		}
+	}
+}
+
+void PrintMessage(char* Word,int Size)
+{
+	int i,w;
+	int base=Base;
+	int PosY=0x0;
+
+	for(i=0;i<16;i++)
+	{
+		PosY=(((i+1)*80)*2)+6;
+		for(w=0;w<Size;w++)
+		{
+			putInMemory(Base2, base+PosY, Word[w]);
+			putInMemory(Base2, base+1+PosY, i);
+			
+			base+=2;
+		}
+		base-=Size*2;
+	}	
+}
 
 
